0e204cba6f8c11628fc2e023d3403353840fa965
Jotappe <jotappe-@hotmail.com>
2022-02-14 21:27:38 +0000

    Sidebar component config

--- src/App.tsx
+++ src/App.tsx
@@ -20,26 +20,14 @@ interface GenreResponseProps {
 }
 
 
-
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
   const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
 
-  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
 
 
 
   useEffect(() => {
-    api.get<GenreResponseProps[]>('genres').then(response => {
-      setGenres(response.data);
-    });
-  }, []);
-
-  useEffect(() => {
-    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
-      setMovies(response.data);
-    });
-
     api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
       setSelectedGenre(response.data);
     })
@@ -49,10 +37,12 @@ export function App() {
     setSelectedGenreId(id);
   }
 
+
+
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
 
-      <SideBar />
+      <SideBar selectedGenreId={selectedGenreId} handleClickButton={handleClickButton} />
       <Content selectedGenre={selectedGenre} selectedGenreId={selectedGenreId} />
     </div >
   )
